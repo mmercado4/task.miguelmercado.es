2b565509f3a7f4217f3248e7398698b011b4f862
MIGUEL MERCADO SALGADO <mmercado@accom.local>
2022-07-12 19:40:21 +0200

    feat(app): add new list form

--- src/components/Manager/index.vue
+++ src/components/Manager/index.vue
@@ -1,24 +1,48 @@
 <template>
   <main>
     <h2>Listas</h2>
-    <AnalogButtonVue text="Púlsame" :action="unaAccion" />
+    <div class="add-list-zone">
+      <input
+        type="text"
+        class="add-list-input"
+        :class="{ active: showForm }"
+        placeholder="Add new list"
+        v-model="newList"
+      />
+      <RoundButtonVue title="Add list" :action="handleClick" :active="showForm"
+        ><IconBase width="25" height="25"><IconPlus /></IconBase>
+      </RoundButtonVue>
+    </div>
+
     <ListsVue />
   </main>
 </template>
 
 <script>
 import ListsVue from "./Lists";
-import AnalogButtonVue from "../Templates/AnalogButton.vue";
+import RoundButtonVue from "../Templates/RoundButton.vue";
+import IconBase from "../Templates/IconBase.vue";
+import IconPlus from "../Icons/IconPlus.vue";
 
 export default {
   name: "ManagerVue",
   components: {
     ListsVue,
-    AnalogButtonVue,
+    RoundButtonVue,
+    IconBase,
+    IconPlus,
+  },
+  data() {
+    return {
+      showForm: false,
+      newList: "",
+    };
   },
   methods: {
-    unaAccion() {
-      console.log("pulsado");
+    handleClick() {
+      let newStatus = !this.showForm;
+      if (!newStatus) setTimeout(() => (this.newList = ""), 500);
+      this.showForm = newStatus;
     },
   },
 };
@@ -27,5 +51,37 @@ export default {
 <style scoped>
 main {
   min-height: calc(100vh - 100px);
+  position: relative;
+}
+
+.add-list-zone {
+  position: fixed;
+  right: 30px;
+  bottom: 60px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  flex-direction: column;
+  overflow-x: hidden;
+  padding: 5px;
+}
+
+.add-list-input {
+  margin-bottom: 20px;
+  font-family: var(--primary-font);
+  padding: 10px;
+  border: none;
+  border-bottom: 1px solid var(--secondary-color);
+  background-color: transparent;
+  transform: translateX(300px);
+  transition: transform 0.5s ease;
+  outline: none;
+  font-size: 1.1em;
+  text-align: end;
+}
+
+.add-list-input.active {
+  transform: translateX(0);
+  transition: transform 0.5s ease;
 }
 </style>
---
+++ src/components/Templates/RoundButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    :class="{ active: active }"
+    :title="title"
+    @click="action"
+    class="round-button"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "RoundButtonVue",
+  props: {
+    title: String,
+    action: Function,
+    active: Boolean,
+    iconPositionWhileActive: String,
+  },
+};
+</script>
+
+<style>
+button.round-button {
+  width: 60px;
+  background-color: var(--primary-color);
+  color: var(--white);
+  height: 60px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.2);
+}
+
+button.round-button > svg {
+  transform: rotate(0deg);
+  transition: transform 200ms;
+}
+
+button.active > svg {
+  transform: rotate(45deg);
+  transition: transform 200ms;
+}
+</style>
